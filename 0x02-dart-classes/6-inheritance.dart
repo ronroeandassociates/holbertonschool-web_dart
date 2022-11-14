33dda228f1c09ebf1227640a6cd09efa89456a3a
Ronroeandassociates <3321@holbertonschool.com>
2022-11-14 12:18:28 -0600

    try again

--- 0x02-dart-classes/6-inheritance.dart
+++ 0x02-dart-classes/6-inheritance.dart
@@ -7,13 +7,13 @@ class User extends Password {
   double height;
   String user_password;
 
-  User({
-    required this.id,
-    required this.name,
-    required this.age,
-    required this.height,
-    required this.user_password,
-  }) : super(password: user_password);
+  User(
+      {required this.id,
+      required this.name,
+      required this.age,
+      required this.height,
+      required this.user_password})
+      : super(password: user_password);
 
   Map toJson() {
     return {
@@ -26,15 +26,44 @@ class User extends Password {
 
   static User fromJson(Map<dynamic, dynamic> userJson) {
     return User(
-        id: userJson['id'],
-        name: userJson['name'],
-        age: userJson['age'],
-        height: userJson['height'],
-        user_password: userJson['user_password']);
+      id: userJson['id'],
+      name: userJson['name'],
+      age: userJson['age'],
+      height: userJson['height'],
+      user_password: userJson['user_password'],
+    );
   }
 
   @override
   String toString() {
-    return 'User(id: $id, name: $name, age: $age, height: $height, Password: ${isValid(user_password)})';
+    return 'User(id : $id ,name: $name,  age: $age, height: $height, Password: ${isValid()}';
   }
+}
+
+void main() {
+  final djo = User(
+      id: 1, name: "Djo", age: 25, height: 1.89, user_password: "Azert23defde");
+  print('<===========Json=========>:');
+  print('\n');
+  print(djo.toJson());
+  Map map = {
+    'id': 3,
+    'name': 'Youssef',
+    'age': 26,
+    'height': 1.9,
+    'user_password': "Azert23defde"
+  };
+  final youssef = User.fromJson(map);
+  print('\n');
+  print('<===========Test1===========>:');
+  print('\n');
+  print(djo.toString());
+  print(youssef.toString());
+  print('\n');
+  print('<===========Test2===========>:');
+  print('\n');
+  djo.user_password = "holberton98";
+  youssef.user_password = "AZERfghn6789";
+  print(djo.toString());
+  print(youssef.toString());
 }
