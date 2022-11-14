799cd2ead20cef8be5be246288e17891d493b477
Ronroeandassociates <3321@holbertonschool.com>
2022-11-14 07:59:49 -0600

    task 4

--- 0x00-hello-dart/4-print_string.dart
+++ 0x00-hello-dart/4-print_string.dart
@@ -1,5 +1,4 @@
-//print 3 times a string stored in the variable str, followed by its first 9 characters
 void main() {
   String str = "Holberton School";
-  print('${str}${str}${str}\n${str.substring(0, 9)}');
+  print('${str}${str}${str}\n${str.substring(0, 9)}\n');
 }
