4c85b17f0f691d4e91ab6354a665c368476354f0
Ronroeandassociates <3321@holbertonschool.com>
2022-11-16 11:00:41 -0600

    try to fix it

--- 0x03-asynchronous/Quotes/quotes.dart
+++ 0x03-asynchronous/Quotes/quotes.dart
@@ -7,8 +7,8 @@ Future<String> generateQuote(id) async {
     http.Response resp =
         await http.get(Uri.parse('https://breakingbadapi.com/api/quotes/$id'));
     var quotes = jsonDecode(resp.body);
-    return "${quotes[0]['author']}: ${quotes[0]['quote']}";
+    return "${quotes[0]['author']} : ${quotes[0]['quote']}";
   } catch (err) {
-    return "There is no quotes";
+    return "There are no quotes";
   }
 }
